deae1f1e24ceb0ae37fdcbafe697d273c7b59898
AnisiaWong <113358877+AnisiaWong@users.noreply.github.com>
2022-11-08 22:07:37 -0500

    Update menu.dart

--- lib/menu.dart
+++ lib/menu.dart
@@ -17,45 +17,45 @@ class Menu {
     'Monday',
     'assets/mon1.jpg',
     [
-      Food('Sweet Potato Oatmeal Muffin', 'assets/mon1.jpg'),
-      Food('New York Yogurt Choice', 'assets/mon2.jpg'),
-      Food('Hot Oatmeal', 'assets/oatmeal.jpg'),
-      Food('Seasonal Fresh Fruit', 'assets/fruit.jpg'),
+      Food('Sweet Potato Oatmeal Muffin'),
+      Food('New York Yogurt Choice'),
+      Food('Hot Oatmeal'),
+      Food('Seasonal Fresh Fruit'),
     ],
   ),
   Menu(
     'Tuesday',
     'assets/tue1.jpg',
     [
-      Food('Cinnamon Burst Pancakes', 'assets/tue1.jpg'),
-      Food('Breakfast Bar', 'assets/breakfastbar.jpg'),
-      Food('Seasonal Fresh Fruit', 'assets/fruit.jpg'),
+      Food('Cinnamon Burst Pancakes'),
+      Food('Breakfast Bar'),
+      Food('Seasonal Fresh Fruit'),
     ],
   ),
   Menu(
     'Wednesday',
     'assets/wed1.jpg',
     [
-     Food('Honey Corn Breakfast Bread', 'assets/wed1.jpg'),
-     Food('Hot Oatmeal', 'assets/oatmeal.jpg'),
-     Food('Seasonal Fresh Fruit', 'assets/fruit.jpg'),
+     Food('Honey Corn Breakfast Bread'),
+     Food('Hot Oatmeal'),
+     Food('Seasonal Fresh Fruit'),
     ],
   ),
   Menu(
     'Thursday',
     'assets/thur1.jpg',
     [
-      Food('Turkey Sausage with Cheese on an English Muffin', 'assets/thur1.jpg'),
-      Food('Breakfast Bar', 'assets/breakfastbar.jpg'),
-      Food('New York Apples Slices', 'assets/thur2.jpg'),
+      Food('Turkey Sausage with Cheese on an English Muffin'),
+      Food('Breakfast Bar'),
+      Food('New York Apples Slices'),
     ],
   ),
   Menu(
     'Friday',
     'assets/fri1.jpg',
     [
-      Food('Assorted Fresh Bagels with Cream Cheese and Jelly', 'assets/fri1.jpg'),
-      Food('Seasonal Fresh Fruit', 'assets/fruit.jpeg'),
+      Food('Assorted Fresh Bagels with Cream Cheese and Jelly'),
+      Food('Seasonal Fresh Fruit'),
     ],
   ),
 ];
@@ -64,18 +64,9 @@ class Menu {
 
 class Food {
   String food;
-  String asset;
 
   Food(
     this.food,
-    this.asset,
   );
-
-  String getFood (){
-    return food;
-  }
-  String getAsset (){
-    return asset;
-  }
 }
 
